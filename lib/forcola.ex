727220602feb29663282d4e518fe37c3ab86b163
Josh Rotenberg <joshrotenberg@gmail.com>
2026-07-02 19:44:00 -0700

    chore: initialize mix project

---
+++ lib/forcola.ex
@@ -0,0 +1,18 @@
+defmodule Forcola do
+  @moduledoc """
+  Documentation for `Forcola`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Forcola.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/forcola_test.exs
@@ -0,0 +1,8 @@
+defmodule ForcolaTest do
+  use ExUnit.Case
+  doctest Forcola
+
+  test "greets the world" do
+    assert Forcola.hello() == :world
+  end
+end
